fc1fe603940dbb03218727336eb3320c51ef117d
Ray Kim(winsrv_wsl) <blastak@naver.com>
2025-07-23 16:12:35 +0900

    fix: Allow multiple inputs for QBB module in parse_model

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1735,7 +1735,7 @@ def parse_model(d, ch, verbose=True):
         elif m is Concat:
             c2 = sum(ch[x] for x in f)
         elif m in frozenset(
-            {Detect, WorldDetect, YOLOEDetect, Segment, YOLOESegment, Pose, OBB, ImagePoolingAttn, v10Detect}
+            {Detect, WorldDetect, YOLOEDetect, Segment, YOLOESegment, Pose, OBB, QBB, ImagePoolingAttn, v10Detect}
         ):
             args.append([ch[x] for x in f])
             if m is Segment or m is YOLOESegment:
